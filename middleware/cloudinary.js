c12755277579c516d5178ecc881af43246f2c48d
kingpoi06 <putratyo90@gmail.com>
2024-11-10 23:44:18 +0800

    fix data done

--- middleware/cloudinary.js
+++ middleware/cloudinary.js
@@ -1,9 +1,9 @@
 import { v2 as cloudinary } from 'cloudinary';
 
 cloudinary.config({ 
-    cloud_name: 'sahabatmengeruda', 
-    api_key: '422333247885175', 
-    api_secret: 'n1sLSaixfks6HZ59gtBheTDnhg'
+    cloud_name: 'df5vgjajn', 
+    api_key: '894181979718297', 
+    api_secret: 'dxBrA66vY09hwSU84MqXnKcuoVg'
 });
 
 export default cloudinary;
